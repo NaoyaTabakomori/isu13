7cc6a0c361620c355f13b1e128cbb951b1f8ffc3
lazydg <1732998+lazydg@users.noreply.github.com>
2023-11-25 11:54:32 +0900

    add index

--- sql/initdb.d/10_schema.sql
+++ sql/initdb.d/10_schema.sql
@@ -102,5 +102,6 @@ CREATE TABLE `reactions` (
   `livestream_id` BIGINT NOT NULL,
   -- :innocent:, :tada:, etc...
   `emoji_name` VARCHAR(255) NOT NULL,
-  `created_at` BIGINT NOT NULL
+  `created_at` BIGINT NOT NULL,
+  INDEX `idx_livestream_id` (`livestream_id`)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
